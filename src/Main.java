31fe46bf0ed04d020a3b9b6aa9d8f31c8b2972c3
raylpz <rayanelopezii@gmail.com>
2023-10-11 19:40:17 -0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main (String [] args) {
+        //Ficha de treino
+        int agachamento = 1;
+        for (i=0; i++; i<=14){
+            for (i=0; i++; i<=2){
+                  System.out.println ("Parabéns! Você concluiu o exercício de agachamento");
+                }
+            }
+        }
+    }
+}
