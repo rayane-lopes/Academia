391d177f96c8c341f71cf02f75dd3b1928d26c90
raylpz <rayanelopezii@gmail.com>
2023-10-20 05:37:00 -0300

    update changes

---
+++ src/TreinoWhile.java
@@ -0,0 +1,12 @@
+public class TreinoWhile {
+    public void treinoWhile (){
+    //quantidade de dias para o primeiro ciclo do treino de adaptação
+        System.out.println("\nQuantidade de dias do treino de adaptação:");
+    int adaptacao = 1;
+        while (adaptacao <= 20) {
+        System.out.println ("\nDia: " + adaptacao + " concluído com sucesso." );
+        adaptacao++;
+    }
+        System.out.println("\nParabéns! Você concluiu seu treino de adaptação.");
+ }
+}
