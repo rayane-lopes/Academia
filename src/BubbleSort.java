5e7678e79fd0624fdf415eef1cc579465af858f1
raylpz <rayanelopezii@gmail.com>
2023-10-24 05:53:36 -0300

    update changes

---
+++ src/BubbleSort.java
@@ -0,0 +1,11 @@
+public class BubbleSort {
+    public static void bubbleSort (String [] args){
+        int myList [] = {2,4,1,3,5};
+        int aux;
+        // "i" é a quantidade de repetição do meu laço for
+        // ?? não deveria ser i <= myList.length ??
+        for (int i = 0; i < myList.length; i++){
+
+        }
+    }
+}
