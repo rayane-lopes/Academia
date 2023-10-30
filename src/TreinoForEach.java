0c6e14fa73ed5e4ad05391f8ac1c6b7bc33d5571
raylpz <rayanelopezii@gmail.com>
2023-10-30 05:54:51 -0300

    update changes

---
+++ src/TreinoForEach.java
@@ -0,0 +1,10 @@
+public class TreinoForEach {
+    public void treinoForEach (){
+        //tomar whey e creatina todos os dias da semana
+        System.out.println("\nQuais dias devo tomar Whey Protein e Creatina?\n");
+        String[] dias = {"Segunda","Terça","Quarta","Quinta","Sexta","Sábado","Domingo"};
+        for (String i : dias) {
+            System.out.println(i);
+        }
+    }
+}
